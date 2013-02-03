bf457818badc3b413ad380d9d3b41185952d3fd3
Stas Boukarev <stassats@gmail.com>
2013-02-04 01:39:54 +0400

    Optimize make-optimized, don't cons constant arguments.

--- primitive-call.lisp
+++ primitive-call.lisp
@@ -77,48 +77,66 @@
           (finally
            (return (values types clean-forms))))))
 
-(defun make-optimized (instance method &key instance-resolver args resolver)
+(defun make-optimized (instance method
+                       &key instance-resolver args resolver
+                            env)
   (multiple-value-bind (fix-types args) (parse-optimized-call-args args)
     (let ((argsyms (make-symbols 'arg (length args)))
-          (sigsyms (make-symbols 'sig (length args))))
-      `(let ((instance ,(funcall instance-resolver instance))
-             (method ,method)
-             (types ',fix-types)
-             ,@(iter (for arg in args)
-                     (for sym in argsyms)
-                     (collect `(,sym ,arg))))
-         (let ((args (list ,@argsyms))
-               ,@(iter (for sig in sigsyms)
-                       (for arg in argsyms)
-                       (collect `(,sig (signature-type ,arg)))))
-           (multiple-value-bind (instance-qclass instance-extra-sig)
-               (typecase instance
-                 (integer
-                  (values instance :static))
-                 (dynamic-object
-                  (values (qobject-class instance)
-                          (class-generation (class-of instance))))
-                 (t
-                  (values (qobject-class instance) :instance)))
-             (cached-values-bind (fun) ,resolver
-                 ((instance-qclass :hash t)
-                  (instance-extra-sig)
-                  (method)
-                  ,@(loop for sig in sigsyms
-                          collect `(,sig :hash sxhash)))
-               (funcall fun instance args))))))))
+          (sigsyms (make-symbols 'sig
+                                 (loop for arg in args
+                                       count (not (constantp arg env))))))
+      `(let* (,@(iter (for arg in args)
+                  (for sym in argsyms)
+                  (unless (constantp arg env)
+                    (collect `(,sym ,arg))))
+              (instance ,(funcall instance-resolver instance))
+              (method ,method)
+              (types ',fix-types)
+              (args ,(if (loop for arg in args
+                               always (constantp arg env))
+                         `',args
+                         `(list ,@(loop for arg in args
+                                        for argsym in argsyms
+                                        collect (if (constantp arg env)
+                                                    arg
+                                                    argsym)))))
+              ,@(loop with sigs = sigsyms
+                      for arg in args
+                      for argsym in argsyms
+                      unless (constantp arg env)
+                      collect `(,(pop sigs) (signature-type ,argsym))))
+         (multiple-value-bind (instance-qclass instance-extra-sig)
+             (typecase instance
+               (integer
+                (values instance :static))
+               (dynamic-object
+                (values (qobject-class instance)
+                        (class-generation (class-of instance))))
+               (t
+                (values (qobject-class instance) :instance)))
+           (cached-values-bind (fun) ,resolver
+               ((instance-qclass :hash t)
+                (instance-extra-sig)
+                (method)
+                ,@(loop for sig in sigsyms
+                        collect `(,sig :hash sxhash)))
+             (funcall fun instance args)))))))
 
-(defmacro optimized-call (allow-override-p instance method &rest args)
+(defmacro optimized-call (allow-override-p instance method &rest args
+                          &environment env)
   (make-optimized instance method
     :instance-resolver #'compile-time-resolve-this
     :args args
-    :resolver `(resolve-call ,allow-override-p instance method args types)))
+    :resolver `(resolve-call ,allow-override-p instance method args types)
+    :env env))
 
-(defmacro optimized-new (class-or-instance &rest args)
+(defmacro optimized-new (class-or-instance &rest args
+                         &environment env)
   (make-optimized class-or-instance nil
     :instance-resolver #'compile-time-resolve-ctor-this
     :args args
-    :resolver `(resolve-new instance args types)))
+    :resolver `(resolve-new instance args types)
+    :env env))
 
 (defun resolve-call (allow-override-p instance method args &optional fix-types)
   ;; (format *trace-output* "cache miss for ~A::~A~%" instance method)
